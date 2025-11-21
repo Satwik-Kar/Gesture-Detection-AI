e932403f90a78139cc9d570f1ffc5559972d9df4
Satwik-Kar <satwik.k.2000@gmail.com>
2025-11-21 17:09:44 +0530

    added more gestures,solved the bug of palm only gesture detection

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ ERASER_THICKNESS = 150
 BRUSH_RADIUS = 130
 HOVER_INDICATOR_RADIUS = 15
 
-COLOR_DRAW = (255, 0, 255)      
-COLOR_ERASER = (0, 0, 0)        
-COLOR_HOVER = (0, 255, 0)       
-COLOR_HEADER_BG = (40, 40, 40)  
+COLOR_DRAW = (255, 0, 255)
+COLOR_ERASER = (0, 0, 0)
+COLOR_HOVER = (0, 255, 0)
+COLOR_HEADER_BG = (40, 40, 40)
 COLOR_TEXT_MAIN = (255, 255, 255)
 COLOR_TEXT_SUB = (200, 200, 200)
 COLOR_TEXT_GESTURE = (255, 255, 0)
@@ -32,8 +32,8 @@ MODE_GESTURE = "Gesture"
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
-    min_detection_confidence=MIN_DETECTION_CONF, 
-    min_tracking_confidence=MIN_TRACKING_CONF, 
+    min_detection_confidence=MIN_DETECTION_CONF,
+    min_tracking_confidence=MIN_TRACKING_CONF,
     max_num_hands=1
 )
 mp_draw = mp.solutions.drawing_utils
@@ -49,30 +49,55 @@ img_canvas = None
 current_mode = MODE_WRITING
 xp, yp = 0, 0
 
-def classify_hand_landmarks(landmarks):
-    thumb_open = landmarks[4].x > landmarks[3].x if landmarks[4].x > landmarks[17].x else landmarks[4].x < landmarks[3].x
-    index_finger_open = landmarks[8].y < landmarks[6].y
-    middle_finger_open = landmarks[12].y < landmarks[10].y
-    ring_finger_open = landmarks[16].y < landmarks[14].y
-    pinky_finger_open = landmarks[20].y < landmarks[18].y
+def get_fingers_status(landmarks):
+    thumb_tip = landmarks[4]
+    thumb_ip = landmarks[3]
+    pinky_mcp = landmarks[17]
 
-    fingers_state = [thumb_open, index_finger_open, middle_finger_open, ring_finger_open, pinky_finger_open]
+    if thumb_tip.x > pinky_mcp.x:
+        thumb_open = thumb_tip.x > thumb_ip.x
+    else:
+        thumb_open = thumb_tip.x < thumb_ip.x
 
-    if all(fingers_state):
+    index_open = landmarks[8].y < landmarks[6].y
+    middle_open = landmarks[12].y < landmarks[10].y
+    ring_open = landmarks[16].y < landmarks[14].y
+    pinky_open = landmarks[20].y < landmarks[18].y
+
+    return [thumb_open, index_open, middle_open, ring_open, pinky_open]
+
+def classify_hand_landmarks(landmarks, fingers):
+    thumb_open, index_open, middle_open, ring_open, pinky_open = fingers
+    thumb_tip = landmarks[4]
+    thumb_ip = landmarks[3]
+
+    if all(fingers):
         return "Open Hand"
-    elif not any(fingers_state):
+    elif not any(fingers):
         return "Fist"
-    elif thumb_open and not any(fingers_state[1:]):
-        return "Thumbs Up" if landmarks[4].y < landmarks[3].y else "Thumbs Down"
-    elif index_finger_open and middle_finger_open and not any(fingers_state[3:]):
+    elif thumb_open and not any(fingers[1:]):
+        if thumb_tip.y < thumb_ip.y:
+            return "Thumbs Up"
+        else:
+            return "Thumbs Down"
+    elif index_open and middle_open and not any(fingers[3:]):
         return "Peace"
-    elif thumb_open and index_finger_open and not any(fingers_state[2:]):
-        return "Okay"
-    elif index_finger_open and pinky_finger_open and not any([middle_finger_open, ring_finger_open]):
-        return "Rock!!"
-    elif index_finger_open and not any([middle_finger_open, ring_finger_open]):
+    elif thumb_open and index_open and not any(fingers[2:]):
+        return "L Sign"
+    elif index_open and pinky_open and not any([middle_open, ring_open]):
+        if thumb_open:
+            return "Spiderman"
+        else:
+            return "Rock!!"
+    elif thumb_open and pinky_open and not any([index_open, middle_open, ring_open]):
+        return "Call Me"
+    elif index_open and middle_open and ring_open and not pinky_open:
+        return "Three"
+    elif index_open and middle_open and ring_open and pinky_open and not thumb_open:
+        return "Four"
+    elif index_open and not any([middle_open, ring_open]):
         return "Pointed"
-    elif middle_finger_open and ring_finger_open and pinky_finger_open and not any([index_finger_open, thumb_open]):
+    elif middle_open and ring_open and pinky_open and not any([index_open, thumb_open]):
         return "Perfect!!"
     else:
         return "Unknown"
@@ -97,9 +122,11 @@ while True:
 
     if current_mode == MODE_WRITING:
         cv2.putText(img, "1 Finger: Write  |  2 Fingers: Hover  |  3 Fingers: Erase", (20, 85), cv2.FONT_HERSHEY_SIMPLEX, 0.8, COLOR_TEXT_SUB, 2)
-    
+
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
+            fingers = get_fingers_status(hand_lms.landmark)
+            
             lm_list = []
             for id, lm in enumerate(hand_lms.landmark):
                 px, py = int(lm.x * w), int(lm.y * h)
@@ -109,32 +136,20 @@ while True:
 
             if len(lm_list) != 0:
                 if current_mode == MODE_GESTURE:
-                    gesture_name = classify_hand_landmarks(hand_lms.landmark)
+                    gesture_name = classify_hand_landmarks(hand_lms.landmark, fingers)
                     cv2.putText(img, f"Gesture: {gesture_name}", (30, 180), cv2.FONT_HERSHEY_SIMPLEX, 1.5, COLOR_TEXT_GESTURE, 3)
                     xp, yp = 0, 0
 
                 elif current_mode == MODE_WRITING:
                     x1, y1 = lm_list[8][1:]
                     
-                    fingers = []
-                    if lm_list[4][1] < lm_list[3][1]: 
-                        fingers.append(1)
-                    else:
-                        fingers.append(0)
-
-                    for id in range(8, 21, 4):
-                        if lm_list[id][2] < lm_list[id - 2][2]:
-                            fingers.append(1)
-                        else:
-                            fingers.append(0)
-
                     if fingers[1] and fingers[2] and fingers[3]:
                         cv2.circle(img, (x1, y1), BRUSH_RADIUS, COLOR_ERASER, cv2.FILLED)
                         cv2.putText(img, "Eraser", (x1+35, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLOR_ERASER, 1)
-                        
+
                         if xp == 0 and yp == 0:
                             xp, yp = x1, y1
-                        
+
                         cv2.line(img_canvas, (xp, yp), (x1, y1), COLOR_ERASER, ERASER_THICKNESS)
                         xp, yp = x1, y1
 
@@ -144,10 +159,10 @@ while True:
 
                     elif fingers[1] and not fingers[2]:
                         cv2.circle(img, (x1, y1), HOVER_INDICATOR_RADIUS, COLOR_DRAW, cv2.FILLED)
-                        
+
                         if xp == 0 and yp == 0:
                             xp, yp = x1, y1
-                        
+
                         cv2.line(img_canvas, (xp, yp), (x1, y1), COLOR_DRAW, BRUSH_THICKNESS)
                         xp, yp = x1, y1
                     else:
@@ -162,17 +177,14 @@ while True:
     img = cv2.bitwise_or(img, img_canvas)
 
     cv2.imshow(WINDOW_NAME, img)
-    
+
     key = cv2.waitKey(1)
     if key == KEY_QUIT_Q or key == KEY_QUIT_ESC:
         break
     elif key == KEY_CLEAR:
         img_canvas = np.zeros((h, w, 3), np.uint8)
     elif key == KEY_MODE:
-        if current_mode == MODE_WRITING:
-            current_mode = MODE_GESTURE
-        else:
-            current_mode = MODE_WRITING
+        current_mode = MODE_GESTURE if current_mode == MODE_WRITING else MODE_WRITING
 
 cap.release()
 cv2.destroyAllWindows()
